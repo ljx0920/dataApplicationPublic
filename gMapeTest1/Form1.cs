71328ef6421baa773def637d4f555b4de5b10d9d
admin <1459223114@qq.com>
2019-03-26 09:59:38 +0800

    20190326 jy

--- gMapeTest1/Form1.cs
+++ gMapeTest1/Form1.cs
@@ -3,9 +3,11 @@ using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -85,11 +87,12 @@ namespace gMapeTest1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.dataBind();
+            //this.dataBind();
+            this.dataGridView1.Show();
             //2. 调用类的初始化方法，记录窗体和其控件的初始位置和大小
             asc.controllInitializeSize(this);
             //标记
-            RadaMarker();
+            RadarMarker();
         }
 
         //3.为窗体添加SizeChanged事件，并在其方法Form1_SizeChanged中，调用类的自适应方法，完成自适应  
@@ -98,23 +101,17 @@ namespace gMapeTest1
             asc.controlAutoSize(this);
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         //radar标注，添加图层
-        public void RadaMarker()
+        public void RadarMarker()
         {
-            GMapOverlay A =new GMapOverlay("A");
-            GMapOverlay B = new GMapOverlay("B");
+            GMapOverlay RadarOverlay =new GMapOverlay("RadarOverlay");
             string path = "F:\\VsWorkspace\\gMapeTest1\\gMapeTest1\\Image\\radar1.jpg";
-            addMarker(path, A, new PointLatLng(30, 104));
-            addMarker(path, A, new PointLatLng(31, 105));
+            addMarker(path, RadarOverlay, new PointLatLng(30, 104));
+            addMarker(path, RadarOverlay, new PointLatLng(31, 105));
         }
-        //图层添加方法
+       
+        //标注图层添加方法
         private void addMarker(string RadarPath, GMapOverlay markBrand, PointLatLng point)
-
         {
             Bitmap bitmap = null;
             bitmap = Bitmap.FromFile(RadarPath) as Bitmap;
@@ -122,6 +119,130 @@ namespace gMapeTest1
             markBrand.Markers.Add(marker);
             this.gMapControl1.Overlays.Add(markBrand);
         }
+       
+        //划线
+        public void Line()
+        {
+            GMapOverlay DirecLine = new GMapOverlay("DirecLine");
+            List<double> StartListLat = new List<double>();
+            StartListLat.Add(30);
+            StartListLat.Add(30);
+            StartListLat.Add(30);
+            StartListLat.Add(30);
+            List<double> StartListLng = new List<double>();
+            StartListLng.Add(104);
+            StartListLng.Add(104);
+            StartListLng.Add(104);
+            StartListLng.Add(104);
+            List<double> TerminalListLat = new List<double>();
+            TerminalListLat.Add(33);
+            TerminalListLat.Add(33);
+            TerminalListLat.Add(33);
+            TerminalListLat.Add(33);
+            List<double> TerminalListLng = new List<double>();
+            TerminalListLng.Add(105);
+            TerminalListLng.Add(115);
+            TerminalListLng.Add(125);
+            TerminalListLng.Add(130);
+            for (int i = 0; i < StartListLat.Count; i++)
+            {
+                PointLatLng StartP = new PointLatLng(StartListLat[i], StartListLng[i]);
+                PointLatLng TerminalP = new PointLatLng(TerminalListLat[i], TerminalListLng[i]);
+                DrawDirectionLine(DirecLine, StartP, TerminalP);
+            }
+
+        }
+        
+        //划线方法
+        public void DrawDirectionLine(GMapOverlay DirecLine, PointLatLng StartP, PointLatLng TerminalP)
+        {
+            List<PointLatLng> list = new List<PointLatLng>();
+            list.Add(StartP);
+            list.Add(TerminalP);
+            GMapPolygon line = new GMapPolygon(list, "line");
+            DirecLine.Polygons.Add(line);
+            this.gMapControl1.Overlays.Add(DirecLine);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Line();
+        }
+
+        //从excel导入数据
+        public static DataTable ReadExcelToTable(string path)//excel存放的路径
+        {
+            try
+            {
+                ////连接字符串
+                //// Office 07及以上版本 不能出现多余的空格 而且分号注意
+                //string connstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';";
+                ////Office 07以下版本
+                ////string connstring = Provider=Microsoft.JET.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';";  
+                string strExtension = System.IO.Path.GetExtension(path);
+                string strFileName = System.IO.Path.GetFileName(path);
+                String connstring;
+                switch (strExtension)
+                {
+                    case ".xls":
+                        connstring = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";" + "Extended Properties=\"Excel 8.0;HDR=yes;IMEX=1;\"";
+                        break;
+                    case ".xlsx":
+                        connstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";" + "Extended Properties=\"Excel 12.0;HDR=yes;IMEX=1;\"";//此连接可以操作.xls与.xlsx文件 (支持Excel2003 和 Excel2007 的连接字符串)  备注： "HDR=yes;"是说Excel文件的第一行是列名而不是数，"HDR=No;"正好与前面的相反。"IMEX=1 "如果列中的数据类型不一致，使用"IMEX=1"可必免数据类型冲突。 
+                        break;
+                    default:
+                        connstring = null;
+                        break;
+                }
+
+                using (OleDbConnection conn = new OleDbConnection(connstring))
+                {
+                    conn.Open();
+                    DataTable sheetsName = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "Table" }); //得到所有sheet的名字
+                    string firstSheetName = sheetsName.Rows[0][2].ToString(); //得到第一个sheet的名字
+                    string sql = string.Format("SELECT * FROM [{0}]", firstSheetName); //查询字符串                    //string sql = string.Format("SELECT * FROM [{0}] WHERE [日期] is not null", firstSheetName); //查询字符串
+                    OleDbDataAdapter ada = new OleDbDataAdapter(sql, connstring);
+                    DataSet set = new DataSet();
+                    ada.Fill(set);
+                    return set.Tables[0];
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            String path = file.FileName;
+            //String path = "C:\\Users\\admin\\Desktop\\数据分析工具测试数据.xlsx";
+            DataTable dt = ReadExcelToTable(path);
+            //DataTable dt2 = new DataTable();
+            
+            ////提取第一列作为表头
+            //for (int i = 0; i < dt.Columns.Count; i++)
+            //{
+            //    dt2.Columns.Add(new DataColumn(dt.Rows[0][i].ToString()));
+            //}
+            ////填充dt2
+            //for (int i = 1; i < dt.Rows.Count; i++)
+            //{
+            //    //dt2.Rows.Add(dt.Rows[i]) ;
+            //    DataRow dr = dt2.NewRow();
+            //    for (int j = 0; j < dt.Columns.Count; j++)
+            //    {
+            //        dr[dt2.Columns[j].ColumnName] = dt.Rows[i][j];
+            //    }
+            //    dt2.Rows.Add(dr);
+            //    //DataRow dr = dt.Rows[i];
+            //    //dt2.ImportRow(dt.Rows[i]);
+            //}
+            this.dataGridView1.DataSource = dt;
+        }
+
 
         //class GMapMarkerImage : GMapMarker
         //{
